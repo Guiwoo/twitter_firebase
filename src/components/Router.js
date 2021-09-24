6653f8327158c379d8e1ae7ac26d89a6492078a3
Guiwoo <park.guiwoo@hotmail.com>
2021-09-24 13:37:16 +0900

    hash

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import CreateAcc from "routes/CreateAcc";
 import Missing from "routes/Missing";
 import Profile from "routes/Profile";
